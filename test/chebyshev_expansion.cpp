20f3ae91aedc53e07e0f920108402508bd7e86f2
Michel Romero <studentenherz@gmail.com>
2022-03-26 03:13:12 -0300

    Add Chebyshev expansion tests
    The Chebyshev expansion was failing because of the error inside
    interpolations that was solved by commit e932c01. I added a test to
    Chebyshev expansion that also requires interpolations to be working
    properly. However, the results with the expansion are not very good
    the accuracy is about 0.02 for order 23 and doesn't increase with
    higher orders, in fact, the best result comparing element by element
    is obtained for order 13. This might have to do with the fact that
    we are interpolating first in order to make the expansion.

--- test/chebyshev_expansion.cpp
+++ test/chebyshev_expansion.cpp
@@ -5,7 +5,10 @@
 #include "files.hpp"
 
 int main(int argc, char const *argv[]){
-	if (argc < 3) return 0;
+	if (argc < 4){
+		std::cout << "Usage:\nchebyshev_expansion <file_in> <order> <error>\n";
+		return 0;
+	}
 
 	// Raw EFIT matrix
 	Matrix2D<double> M;
@@ -14,7 +17,6 @@ int main(int argc, char const *argv[]){
 
 	// Chebyshev coefficients vector
 	size_t n = atoi(argv[2]);
-	std::cout << n << '\n';
 	Matrix2D<double> a(n + 1, n + 1);
 
 	// Just to set some values,
@@ -37,19 +39,23 @@ int main(int argc, char const *argv[]){
 		double x = x_min + i * (x_max - x_min) / Nx;
 		for (size_t j = 0; j < Ny; j++){
 			double y = y_min + j * (y_max - y_min) / Ny;
-			new_M(i, j) = evaluate_Chebyshev_T_expansion(a, x, y, x_min, x_max, y_min, y_max);
+			new_M(i, j) = evaluate_Chebyshev_T_expansion(n, a, x, y, x_min, x_max, y_min, y_max);
 		}
 	}
 
 	// Compare
+	double epsilon = atof(argv[3]);
+	double max_diff = 0;
 	for (size_t i = 0; i < Nx; i++)
-		for (size_t j = 0; j < Ny; j++)
-			if (std::abs(M(i, j) - new_M(i, j) > 1e-6)){
+		for (size_t j = 0; j < Ny; j++){
+			max_diff = std::max(max_diff, std::abs(M(i, j) - new_M(i, j)));
+			if (max_diff > epsilon){
 				std::cerr << "At position (" << i << ", " << j << ") got " << new_M(i, j) << " but expected " << M(i, j) << '\n';
 				return 1;
 			}
+		}
 
-
+	std::cout << "Max difference: " << max_diff << '\n';
 
 	return 0;
 }
