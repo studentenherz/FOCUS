c0f0e3b5c19098a0832d5f312564b982e7f69d94
Michel Romero <studentenherz@gmail.com>
2022-04-27 09:21:13 -0300

    Add scalar field struct

---
+++ includes/types/scalarField.hpp
@@ -0,0 +1,22 @@
+#if !defined(FOCUS_INCLUDES_TYPES_SCALAR_FIELD_HPP)
+#define FOCUS_INCLUDES_TYPES_SCALAR_FIELD_HPP
+
+#include "types/matrix2D.hpp"
+
+struct ScalarField{
+	Matrix2D<double>* M;
+	double x_min;
+	double x_max;
+	double y_min;
+	double y_max;
+
+	ScalarField(Matrix2D<double>* _M, double _x_min, double _x_max, double _y_min, double _y_max): M(_M), x_min(_x_min), x_max(_x_max), y_min(_y_min), y_max(_y_max) {}
+
+	double operator()(int i, int j) const{
+		return M->operator()(i, j);
+	}
+};
+
+#endif // FOCUS_INCLUDES_TYPES_SCALAR_FIELD_HPP
+
+
