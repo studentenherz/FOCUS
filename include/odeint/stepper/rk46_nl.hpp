14332119ddd4394f119dbf5facb8e0f8185c30f1
Michel Romero <studentenherz@gmail.com>
2022-05-13 20:41:00 -0300

    Add low Runge-Kutta method stepper
    Low-dissipation and low-dispersion fourth-order Runge-Kutta algorithm
    from https://acoustique.ec-lyon.fr/publi/berland_cfluids06.pdf. Energy
    is conserved with this method and not with Euler.

---
+++ include/odeint/stepper/rk46_nl.hpp
@@ -0,0 +1,29 @@
+#if !defined(FOCUS_INCLUDE_ODEINT_STEPPER_RK46_NL_HPP)
+#define FOCUS_INCLUDE_ODEINT_STEPPER_RK46_NL_HPP
+
+/**
+ * Low-dissipation and low-dispersion fourth-order Runge–Kutta algorithm
+ * (https://acoustique.ec-lyon.fr/publi/berland_cfluids06.pdf)
+ */
+template<typename system_type, typename state_type, typename scalar_type>
+class RK46NL{
+	scalar_type a[6] = {0.000000000000, -0.737101392796, -1.634740794341, -0.744739003780, -1.469897351522, -2.813971388035};
+	scalar_type b[6] = {0.032918605146,  0.823256998200,  0.381530948900,  0.200092213184,  1.718581042715,  0.270000000000};
+	scalar_type c[6] = {0.000000000000,  0.032918605146,  0.249351723343,  0.466911705055,  0.582030414044,  0.847252983783};
+public:
+	RK46NL() {}
+	void do_step(system_type sys, state_type& x, scalar_type t, scalar_type dt){
+		state_type dxdt, xx; // this assumes default constructor gives a zero state
+
+		for (size_t i = 0; i < 6; i++){
+			scalar_type tt = t + c[i] * dt;
+			sys(x, dxdt, tt); // dxdt = f(x, t)
+
+			xx = a[i] * xx + dxdt * dt;
+			x = x + b[i] * xx;
+		}
+
+	}
+};
+
+#endif // FOCUS_INCLUDE_ODEINT_STEPPER_RK46_NL_HPP
--- test/odeint.cpp
+++ test/odeint.cpp
@@ -1,32 +1,52 @@
 #include <iostream>
+#include <fstream>
 
 #include "odeint/integrator.hpp"
 #include "odeint/stepper/euler.hpp"
+#include "odeint/stepper/rk46_nl.hpp"
 #include "types/vector.hpp"
 
-double gam = 0.15;
+double gam = 0.0;
 
 struct System
 {
 	System() {}
 	void operator()(const Vector& x, Vector& dxdt, double t){
 		dxdt[0] = x[1];
-		dxdt[1] = -x[0];
+		dxdt[1] = -x[0] - gam*x[1];
 	}
 };
 
-void observer(Vector v, double t){
-	std::cout << t << '\t' << v << '\n';
-}
+class FileObserver{
+	std::ofstream &_fo;
+public:
+	FileObserver(std::ofstream& fo): _fo(fo) {}
+	
+	void operator()(Vector v, double t){
+		_fo << t << '\t' << v << '\n';
+	}
+};
 
 
 int main(){
 	System sys;
+
 	EulerStepper<System, Vector, double> euler;
+	RK46NL<System, Vector, double> rk46nl;
 
-	Vector x(0, 1, 0);
+	Vector x1(0, 1, 0);
+	Vector x2 = x1;
 
-	integrate(euler, sys, x, 0.0, 0.01, 10000, observer);
+	std::ofstream euler_fo("xy_euler.dat");
+	std::ofstream rk46nl_fo("xy_rk46.dat");
+	FileObserver obs_euler(euler_fo);
+	FileObserver obs_rk46(rk46nl_fo);
+
+	integrate(euler, sys, x1, 0.0, 0.01, 10000, obs_euler);
+	integrate(rk46nl, sys, x2, 0.0, 0.01, 10000, obs_rk46);
+
+	std::cout << "Energy from Euler: " << x1[0] * x1[0] + x1[1] * x1[1] << '\n';
+	std::cout << "Energy from Runge-Kutta: " << x2[0] * x2[0] + x2[1] * x2[1] << '\n';
 
 	return 0;
 }
