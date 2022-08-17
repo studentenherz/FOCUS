c744ff9d89cc9ddecf09dbc8cd25f8874c62f62e
Michel Romero <studentenherz@gmail.com>
2022-08-17 16:36:57 -0300

    Add `cxxopts` for parsing cli arguments
    ninja breaks when I try to build any time it's not a freshly created
    build folder, I don't know if it is because of the new library I added
    or because of something in particular with this computer I'm using now.
    I'll try it on another computer and see.

--- test/random.cpp
+++ test/random.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 #include "random.hpp"
+#include "cxxopts.hpp"
 
 int main(int argc, char* argv[]){
-	if(argc < 2){
-		std::cout << "Usage:\nrandom <seed>\n";
+	cxxopts::Options options("random", "Generate random distributions from seed using ran2");
+
+	typedef unsigned long long ull;
+
+	options.add_options()
+	("s,seed", "Seed of random", cxxopts::value<ull>())
+	("h,help", "Show this help message")
+	("uniform_ofile", "Uniform distribution output file", cxxopts::value<std::string>()->default_value("uniform.dat"));
+	("normal_ofile", "Normal distribution output file", cxxopts::value<std::string>()->default_value("normal.dat"));
+
+	try{
+
+		auto result = options.parse(argc, argv);
+
+		if(result.count("help")){
+			std::cout << options.help() << std::endl;
+			return 0;
+		}
+
+		ull seed = result["seed"].as<ull>();
+		
+		Ran2 ran(seed);
+		std::ofstream uniform(result["uniform_ofile"].as<std::string>());
+		for(int i =0 ; i<10000; i++)
+			uniform << ran.doub() << '\n';
+
+		NormalRand norm(seed, 1.0, 5.0);
+		std::ofstream normal(result["normal_ofile"].as<std::string>());
+		for(int i =0 ; i<10000; i++)
+			normal << norm() << '\n';
+
 		return 0;
+	} catch(cxxopts::OptionException const& e){
+		std::cerr << e.what() << std::endl;
+		return 1;
 	}
-
-	unsigned long long seed = std::atoll(argv[1]);
-	
-	Ran2 ran(seed);
-	std::ofstream uniform("uniform.dat");
-	for(int i =0 ; i<10000; i++)
-		uniform << ran.doub() << '\n';
-
-	NormalRand norm(seed, 1.0, 5.0);
-	std::ofstream normal("normal.dat");
-	for(int i =0 ; i<10000; i++)
-		normal << norm() << '\n';
-
-
-	return 0;
 }
--- test/vector.cpp
+++ test/vector.cpp
@@ -1,27 +1,48 @@
 #include <iostream>
 
 #include "types/vector.hpp"
+#include "cxxopts.hpp"
 
-int main(){
+int main(int argc, char* argv[]){
 	using namespace std;
-	
-	Vector3 zero;
-	Vector3 a = {1.2, 4.0, -9}, b;
-	b = {0, 1, 3};
 
-	cout << "zero = " << zero << '\n';
-	cout << "a = " << a << '\n';
-	cout << "b = " << b << '\n';
+	cxxopts::Options options("vector", "A simple test of vectors in this library");
 
-	cout << "2 * a = " << 2 * a << '\n';
-	cout << "a * (-2) = " << a * (-2) << '\n';
-	cout << "a / 2 = " << a / 2 << '\n';
+	options.add_options()
+		("h,help", "Show this help")
+		("b,bfirst", "First coordinate of b", cxxopts::value<double>()->default_value("0"))
 
-	cout << "a + b = " << a + b << '\n';
-	cout << "a - b = " << a - b << '\n';
-	cout << "dot(a, b) = " << dot(a, b) << '\n';
-	cout << "cross(a, b) = " << cross(a, b) << '\n';
-	cout << "mod(a) = " << mod(a) << '\n';
+	try{
+		auto result = options.parse(argc, argv);
 
-	return 0;
+		if (result.count("help")){
+			cout << options.help() << endl;
+			exit(0);
+		}
+
+		
+		Vector3 zero;
+		Vector3 a = {1.2, 4.0, -9}, b;
+		b = {result["bfirst"].as<double>(), 1, 3};
+
+		cout << "zero = " << zero << '\n';
+		cout << "a = " << a << '\n';
+		cout << "b = " << b << '\n';
+
+		cout << "2 * a = " << 2 * a << '\n';
+		cout << "a * (-2) = " << a * (-2) << '\n';
+		cout << "a / 2 = " << a / 2 << '\n';
+
+		cout << "a + b = " << a + b << '\n';
+		cout << "a - b = " << a - b << '\n';
+		cout << "dot(a, b) = " << dot(a, b) << '\n';
+		cout << "cross(a, b) = " << cross(a, b) << '\n';
+		cout << "mod(a) = " << mod(a) << '\n';
+		return 0;
+	}
+	catch(cxxopts::OptionException const& e){
+		cerr << e.what() << endl;
+		return 1;
+	}
+
 }
