ee553f61d1e1c5f94e29901ac700c5770c763ca1
Michel Romero <studentenherz@gmail.com>
2022-08-19 11:53:35 -0300

    Add 1D interpolaton with Array of x

--- include/interpolations.hpp
+++ include/interpolations.hpp
@@ -45,6 +45,34 @@ double lagrange_interpolation_3(double xi, const Array<double>& f, double x_min,
 	return dot(y, l);
 }
 
+/**
+ * [Lagrange interpolation](https://en.wikipedia.org/wiki/Lagrange_polynomial)
+ * with the 3 closest points.
+ * @param xi value in wich to interpolate
+ * @param xs Array of x values (sorted ascendingly)
+ * @param f Array of corresponding f(x) values
+ * @return f(xi) interpolated from f
+ */
+double lagrange_interpolation_3(double xi, const Array<double>& xs, const Array<double>& f){
+	// Index of closest element to xi to the left
+	size_t index = 1;
+	while (xi > xs[index] && index < xs.size() - 2) index++;
+	
+	Vector3 x, y, l;
+	// Get the points
+	for(size_t k = 0; k < 3; k++){
+		x[k] = xs[index + k - 1];
+		y[k] = f[index + k  - 1];
+	}
+
+	// Basis polynomials evaluated at xi
+	for(size_t k = 0; k < 3; k++)
+		l[k] = (xi - x[(k + 2) % 3])/(x[k] - x[(k + 2) % 3]) * (xi - x[(k + 1) % 3])/(x[k] - x[(k + 1) % 3]);
+				
+	// Linear combination
+	return dot(y, l);
+}
+
 
 /**
  * Interpolate 2D using the 4 points formula.
