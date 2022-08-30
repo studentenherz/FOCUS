d99e75304b4c5ed6dfc12c4d0fadf8926d981c3d
Michel Romero <studentenherz@gmail.com>
2022-08-30 00:15:12 -0300

    Add CUDA to

--- include/types/matrix_2d.hpp
+++ include/types/matrix_2d.hpp
@@ -7,17 +7,22 @@ using shape_t = pair<size_t>;
 
 template <typename T>
 class Matrix2D{
-	T *arr;
+	T *_arr;
 	shape_t _shape;
+	bool _copied;
 public:
 	/**
 	 * Default constructor
 	 * @param n size on the first axis
 	 * @param m size on the second axis
 	 */
+	#ifdef CUDA_BUILD
+	__host__ __device__
+	#endif
 	Matrix2D(size_t n = 0, size_t m = 0){
+		_copied = false;
 		_shape = {n, m};
-		arr = new T[n * m + 1];
+		_arr = new T[n * m + 1];
 	}
 
 	Matrix2D& operator=(const Matrix2D&&) = delete;
@@ -27,27 +32,58 @@ public:
 	 * as a return value from a function sapping the pointers
 	 * and keeping the allocated data on the heap.
 	 */
-	Matrix2D(Matrix2D&& other): _shape(other._shape) {
-		arr = other.arr;
-		other.arr = NULL;
+	#ifdef CUDA_BUILD
+	__host__ __device__
+	#endif
+	Matrix2D(Matrix2D&& other){
+		_shape = other._shape;
+		_arr = other._arr;
+		_copied = other._copied;
+
+		// If the previous matrix was a copied one just copy
+		// else actually do the move
+		if (!_copied)
+			other._arr = NULL;
 	}
+
+	/**
+	 * Construct in host for device from Matrix2D 
+	 * @param other matrix2D to construct from
+	 */
+	#ifdef CUDA_BUILD
+	__host__
+	void construct_in_host_for_device_from_matrix_2d(Matrix2D<T>& other){
+		_copied = true;
+		_shape = other._shape;
+		size_t _size = _shape.first * _shape.second;
+		cudaMalloc(&_arr, sizeof(T) * (_size + 1));
+		cudaMemcpy(_arr, other._arr, sizeof(T) * _size, cudaMemcpyHostToDevice);
+	}
+	#endif
 
 	/**
 	 * Desctructor; dealocate heap and set pointer to null
 	 */
+	#ifdef CUDA_BUILD
+	__host__ __device__
+	#endif
 	~Matrix2D(){
-		delete[] arr;
-		arr = NULL;
+		if (_copied) return;
+		delete[] _arr;
+		_arr = NULL;
 	}
 
 	/** 
 	 * Change shape of matrix droping previous data
 	 * @param n size on the first axis
 	 * @param m size on the second axis
-	 */ 
+	 */
+	#ifdef CUDA_BUILD
+	__host__ __device__
+	#endif 
 	void reshape(size_t n, size_t m){
-		delete[] arr;
-		arr = new T[n * m + 1];
+		delete[] _arr;
+		_arr = new T[n * m + 1];
 		_shape = {n, m};
 	}
 
@@ -55,6 +91,9 @@ public:
 	 * Get shape of the matrix
 	 * @return shape
 	 */
+	#ifdef CUDA_BUILD
+	__host__ __device__
+	#endif
 	shape_t shape() const {
 		return _shape;
 	}
@@ -65,10 +104,13 @@ public:
 	 * @param j index on the second axis
 	 * @return M(i, j)
 	 */
+	#ifdef CUDA_BUILD
+	__host__ __device__
+	#endif
 	T &operator()(size_t i, size_t j){
 		if (i > _shape.first || j > _shape.second)
-			return arr[_shape.first * _shape.second];
-		return arr[i * _shape.second + j];
+			return _arr[_shape.first * _shape.second];
+		return _arr[i * _shape.second + j];
 	}
 	
 	/** 
@@ -77,10 +119,13 @@ public:
 	 * @param j index on the second axis
 	 * @return M(i, j)
 	 */
+	#ifdef CUDA_BUILD
+	__host__ __device__
+	#endif
 	const T &operator()(size_t i, size_t j) const {
 		if (i > _shape.first || j > _shape.second)
-			return arr[_shape.first * _shape.second];
-		return arr[i * _shape.second + j];
+			return _arr[_shape.first * _shape.second];
+		return _arr[i * _shape.second + j];
 	}
 };
 
