83043ff1842b3fbeb2e57edb3e765accce7ca062
Michel Romero <studentenherz@gmail.com>
2022-09-07 02:29:00 -0300

    Add states dump

---
+++ include/formats/particle_states.hpp
@@ -0,0 +1,88 @@
+#if !defined(FOCUS_INCLUDE_FORMATS_PARTICLE_STATES_HPP)
+#define FOCUS_INCLUDE_FORMATS_PARTICLE_STATES_HPP
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include "regex_tokenizer.hpp"
+#include "types/array.hpp"
+#include "types/vector.hpp"
+#include "types/particle.hpp"
+
+/**
+ * Read initial states of particles from a file in the format
+ * r [m] | theta [rad] | z [m] | vr [m/s] | vtheta [m/s] | vz [m/s]
+ *
+ * Comments start with #
+ *  
+ * @param filename input file
+ * @return Array of initial states
+ */
+Array<State> load_states(std::string filename){
+	std::ifstream fi(filename);
+	if(!fi.is_open()){
+		std::cerr << "Couldn't open file " << filename << '\n';
+		return Array<State>(0);
+	}
+
+	Tokenizer<std::ifstream> tk("[+-]?\\d*[\\.]?\\d+(?:[Ee][+-]?\\d+)?"); // captures any number;
+	std::string token;
+
+	std::vector<State> v_states;
+
+	double r, q, z, vr, vq, vz;
+	std::string line;
+	while	(std::getline(fi, line)){
+		if (regex_match(line, "#.*") || line.empty()) continue;
+		std::istringstream iss(line);
+		iss >> r >> q >> z >> vr >> vq >> vz;
+		v_states.push_back({r, q, z, vr, vq, vz});
+	}
+
+	Array<State> states(v_states.size());
+	for (size_t i = 0; i < states.size(); i++)
+		states[i] = v_states[i];
+
+	return states;
+}
+
+/**
+ * Dump states of particles into a file in the format
+ * r [m] | theta [rad] | z [m] | vr [m/s] | vtheta [m/s] | vz [m/s]
+ *
+ * Comments start with #
+ *  
+ * @param filename output file
+ * @param states of the particles
+ * @param part particle species
+ * @return true on success
+ */
+bool dump_states(std::string filename, Array<State>& states, Particle part){
+	std::ofstream fo(filename);
+	if(!fo.is_open()){
+		std::cerr << "Couldn't open file " << filename << '\n';
+		return false;
+	}
+
+	#ifdef GIT_VERSION
+	fo << "# Output file created with FOCUS git version of the code " << GIT_VERSION << '\n';
+	#else
+	fo << "# Output file created with FOCUS git version of the code unknown\n";
+	#endif
+	fo << "# The format of the output is as follows:\n";
+	fo << "# r [m] | theta [rad] | z [m] | vr [m/s] | vtheta [m/s] | vz [m/s]\n\n";
+	fo << "# Particle species:\n";
+	fo << "# Z = " << part.q << '\n';
+	fo << "# m = " << part.m << " Da\n\n";
+	fo << "#States:\n";
+
+	for (size_t i = 0; i < states.size(); i++)
+		fo << states[i] << '\n';
+
+	fo.close();
+	return true;
+}
+
+#endif // FOCUS_INCLUDE_FORMATS_PARTICLE_STATES_HPP
--- test/particle_states.cpp
+++ test/particle_states.cpp
@@ -2,26 +2,29 @@
 #include <string>
 
 #include "cxxopts.hpp"
-#include "formats/input_states.hpp"
+#include "formats/particle_states.hpp"
 #include "types/array.hpp"
 #include "types/vector.hpp"
+#include "types/particle.hpp"
 
 int main(int argc, char* argv[]){
 	cxxopts::options options("input_states", "Test reading input states");
 
 	options.add_options()
-		("input", "", cxxopts::value<std::string>());
+		("input", "", cxxopts::value<std::string>())
+		("output", "", cxxopts::value<std::string>()->default_value("states.out"));
 
-	options.positional_help("<states input file>");
-	options.parse_positional("input");
+	options.positional_help("<states input file> [<states output file>]");
+	options.parse_positional("input", "output");
 
 	try{
 		auto result = options.parse(argc, argv);
 
 		Array<State> states = load_states(result["input"].as<std::string>());
 
-		for(size_t i =0 ; i < states.size(); i++)
-			std::cout << states[i] << '\n';
+		Particle part(1, 1.2);
+
+		dump_states(result["output"].as<std::string>(), states, part);
 
 		return EXIT_SUCCESS;
 	}catch(cxxopts::option_error const& e){
