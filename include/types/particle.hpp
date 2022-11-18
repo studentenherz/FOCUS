e46c463a767780c2a0d1bd936e1eb64eafeafa57
Michel Romero <studentenherz@gmail.com>
2022-11-18 16:44:43 -0300

    Add AtomicPorcess class
    The original idea is to have an AtomicPorcess object for every atomic
    process given by the user and use them on the particles involved. The
    class Particle will have a list of AtomicPorcess's that it can undergo
    and will be applied subjected to the probability of the process occuring

--- include/types/particle.hpp
+++ include/types/particle.hpp
@@ -6,9 +6,10 @@
 #include "types/array.hpp"
 
 struct Particle{
-	uint q;				///< charge
 	double m;			///< mass
+	uint q;				///< charge
 	uint n;				///< Principal quantum number
+	double t;			///< Time since it's been in this quantum state
 
 	#ifdef __CUDACC__
 	__host__ __device__
@@ -24,7 +25,7 @@ struct Particle{
 	#ifdef __CUDACC__
 	__host__ __device__
 	#endif
-	Particle(uint q, double m, uint n = 0): q(q), m(m), n(n) {}
+	Particle(double m, uint q, uint n = 0): m(m), q(q), n(n) {}
 };
 
 #endif // FOCUS_INCLUDE_TYPES_PARTICLE_HPP
