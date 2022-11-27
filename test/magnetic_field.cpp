dd90d4f0d899e7b75c4dbe62ee1fe8ad2f99c9e3
Michel Romero <studentenherz@gmail.com>
2022-11-27 16:22:45 -0300

    Change magnetic_fiels test to output fields

--- test/magnetic_field.cpp
+++ test/magnetic_field.cpp
@@ -1,85 +1,76 @@
 #include <iostream>
 #include <fstream>
 #include <stdlib.h>
+#include <string>
 
 #include "formats/matrix.hpp"
 #include "formats/geqdsk.hpp"
-#include "lorentz.hpp"
 #include "magnetic_field.hpp"
 #include "types/equilibrium.hpp"
 #include "types/vector.hpp"
-#include "odeint/integrator.hpp"
-#include "odeint/stepper/rk46_nl.hpp"
+#include "cxxopts.hpp"
 
-
-class FileObserver{
-	std::ofstream &_fo;
-	double _a, _v0, _Omega, _B0;
-	bool cart;
-	MagneticFieldFromMatrix& _B;
-	FineEquilibrium& fineq;
-public:
-	FileObserver(std::ofstream& fo, double a, double v0, double Omega, MagneticFieldFromMatrix& B, FineEquilibrium& eq, bool cart_coord = false): _fo(fo), _a(a), _v0(v0), _Omega(Omega),  _B0(B.B0()), cart(cart_coord), _B(B), fineq(eq) {}
-	
-	void operator()(State v, double t){
-		// magnetic field 
-		Vector3 B = _B(get_position(v), t);
-		if (cart)
-			_fo << t / _Omega << '\t' << v[0] * cos(v[1]) * _a << ' ' << v[0] * sin(v[1]) * _a << ' ' << v[2] * _a << ' ' << (v[3] * cos(v[1]) - v[4] * sin(v[1])) * _v0 << ' ' << (v[3] * sin(v[1]) + v[3] * cos(v[1])) * _v0 << ' ' << v[5] * _v0 << ' ' << (B[0] * cos(v[1]) - B[1] * sin(v[1])) * _B0 << ' ' << (B[0] * sin(v[1]) + B[1] * cos(v[1])) * _B0 << ' ' << B[2] * _B0 << ' ' << fineq.Psi(v[0], v[2]) << ' ' << fineq.F(v[0], v[2]) << '\n';
-		else
-			_fo << t/ _Omega << '\t' << v[0]  * _a << ' ' << v[1]  << ' ' << v[2] * _a << ' ' << v[3] * _v0 << ' ' << v[4] * _v0 << ' ' << v[5] * _v0 << ' ' << B * _B0 << ' ' << fineq.Psi(v[0], v[2]) << ' ' << fineq.F(v[0], v[2]) << '\n';
-	}
-};
+bool make_dir(std::string dirname){
+	std::string command = "mkdir -p ";
+	command += dirname;
+	return  (system(command.c_str()) == 0);
+}
 
 int main(int argc, char* argv[]){
-	if (argc < 3){
-		std::cout << "usage:\nmagnetic_field <g-eqdsk file> <output_file>\n";
-		return -1;
+	cxxopts::options options("Read from G-EQDSK file and return interpolated magnetif fields and poloidal flux");
+
+	options.add_options()
+		("g,geqdsk", "G-EQDSK file", cxxopts::value<std::string>())
+		("o,output", "Output directory", cxxopts::value<std::string>())
+		("h,help", "Display this help message")
+	;
+
+	try{
+		auto result = options.parse(argc, argv);
+
+		if (result.count("help")){
+			std::cout << options.help() << '\n';
+			exit(0);
+		}
+
+		std::string ifilename = result["geqdsk"].as<std::string>();
+		std::string odirname = result["output"].as<std::string>();
+
+		if (!make_dir(odirname)){
+			std::cerr << "Error creating directory " << odirname << "\n";
+			exit(2);
+		}
+
+		Equilibrium eq = read_geqdsk(ifilename);
+		FineEquilibrium fineq(eq, 26);
+		MagneticFieldMatrix B_matrix(eq, 26, 600);
+
+		std::ofstream interpo_lims(odirname + "/interp_lims.dat");
+		interpo_lims << B_matrix.r_min * eq.rdim << ' ' << B_matrix.r_max * eq.rdim << ' ' << B_matrix.z_min * eq.rdim << ' ' << B_matrix.z_max * eq.rdim << '\n';
+
+		interpo_lims.close();
+
+		dump(odirname + "/Br.dat", B_matrix.Br, false);
+		dump(odirname + "/Bt.dat", B_matrix.Bt, false);
+		dump(odirname + "/Bz.dat", B_matrix.Bz, false);
+		dump(odirname + "/Psi.dat", B_matrix.psi, false);
+
+
+		std::ofstream fobdry(odirname + "/bdry.dat");
+		for (size_t i = 0; i < eq.nbdry; i++)
+			fobdry << eq.rbdry[i] << ' ' << eq.zbdry[i] << '\n';
+		fobdry.close();
+
+		std::ofstream folim(odirname + "/lim.dat");
+		for (size_t i = 0; i < eq.nlim; i++)
+			folim << eq.rlim[i] << ' ' << eq.zlim[i] << '\n';
+		folim.close();
 	}
-
+	catch (cxxopts::option_error const& e){
+		std::cerr << e.what() << '\n';
+		exit(1);
+	}
 	
-	Equilibrium eq = read_geqdsk(argv[1]);
-	FineEquilibrium fineq(eq, 26);
-	MagneticFieldMatrix B_matrix(eq, 26, 600);
-	
-	dump("Br.dat", B_matrix.Br, false);
-	dump("Bt.dat", B_matrix.Bt, false);
-	dump("Bz.dat", B_matrix.Bz, false);
-
-
-	std::cout << B_matrix.r_min * eq.rdim << ' ' << B_matrix.r_max * eq.rdim << ' ' << B_matrix.z_min * eq.rdim << ' ' << B_matrix.z_max * eq.rdim << '\n';
-	std::ofstream fobdry("bdry.dat");
-	for (size_t i = 0; i < eq.nbdry; i++)
-		fobdry << eq.rbdry[i] << ' ' << eq.zbdry[i] << '\n';
-	fobdry.close();
-
-	std::ofstream folim("lim.dat");
-	for (size_t i = 0; i < eq.nlim; i++)
-		folim << eq.rlim[i] << ' ' << eq.zlim[i] << '\n';
-	folim.close();
-	
-	std::cout << "Done\n";
-
-	MagneticFieldFromMatrix B(B_matrix, eq.bcentr);
-	// MagneticField B(fineq);
-
-	double q_over_m =  9.58e7; // C/kg proton
-	double Omega = q_over_m * eq.bcentr; // cyclotron frequency
-	double v0 = 1.84142e7; // m/s (3.54 MeV of a proton)
-	double a = eq.rdim; // m
-	double gam = v0 / (a * Omega); // dimensionless factor
-
-	typedef Lorentz<NullForce, MagneticFieldFromMatrix, NullVectorField> System;
-	Particle part(1, 1);
-	System sys(gam, part, B, null_vector_field, null_force);
-	State x = {2.2 / a, 0, 0, 0.0, 0.01, 0.3};
-	RK46NL<System, State, double> rk46nl;
-
-	std::ofstream fo(argv[2]);
-
-
-	FileObserver obs(fo, a, v0, Omega, B, fineq, false);
-	integrate(rk46nl, sys, x, 0.0, 0.0001, 30000000, obs, 999);
 
 	return 0;
 }
